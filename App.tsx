4e8aa01eb120851720d40f273b3d71a00ece7496
Spencer Benson Njuguna <bennspencer@gmail.com>
2024-04-09 20:34:12 +0300

    connect to db

--- App.tsx
+++ App.tsx
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import {SQLiteProvider} from 'expo-sqlite/next';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import {Asset} from 'expo-asset';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! SP</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const loadDatabase = async () => {
+  const dbName = 'mySQLiteDB.db';
+  const dbAsset = "./assets/mySQLiteDB.db";
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+
+  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+
+  if(!fileInfo.exists){
+    FileSystem.makeDirectoryAsync(
+      `${FileSystem.makeDirectoryAsync}SQLite`,
+      {intermediates:true}
+    );
+    await FileSystem.downloadAsync(dbUri,dbFilePath);
+
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+
+  const [dbLoaded, setDbLoaded] = React.useState<boolean>(false);
+
+  React.useEffect(()=> {
+    loadDatabase()
+    .then(()=> setDbLoaded(true))
+    .catch((e)=>console.log(e))
+  },[]);
+
+  if(!dbLoaded) return (
+  <View style={{flex:1}}>
+    <ActivityIndicator size={"large"}/>
+    <Text>Loading...</Text>;
+    </View>
+  );
+  
+  return (
+   <React.Suspense>
+    <SQLiteProvider databaseName='mySQLiteDB.db' useSuspense={true}>
+
+    </SQLiteProvider>
+   </React.Suspense>
+  );
+};
